87aac14e15910da1fc99096838268e0cb8bd72a3
khadrius <ycanasan@gmail.com>
2024-02-02 18:24:12 +0300

    sad

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        //abstract soyut referans tutucular koyulucak
+        //concrete somut 
+
+    }
+}
